690f0663ea46b658b8c0cab20a83fe394d967175
AndreasHaral <47718412+AndreasHaral@users.noreply.github.com>
2019-02-17 21:25:54 +0200

    Update P18168askisi10.py

--- P18168askisi10.py
+++ P18168askisi10.py
@@ -1,16 +1,16 @@
 import urllib2
-dief=raw_input("Äþóå äéåõèçíóç óåëéäáò ìå http:")
+dief=raw_input("Dwse Url me http:")
 req1=urllib2.Request(dief)
 html=urllib2.urlopen(req1).read()
 sind=0#Gia Sindesmous
 sumbrp=0#Gia Br kai P
 from HTMLParser import HTMLParser
 class class1(HTMLParser):
-    def metritis(self,tag,attrs):
-        if tag=="link" or tag=="a":
+    def metritis(self,tag,attrs):#pairnoyme aparaitita stoixeia apo ton kwdika html
+        if tag=="link" or tag=="a":#gia sindesmous
             global sind
             sind=sind+1
-        if tag=="p" or tag=="br":
+        if tag=="p" or tag=="br":#gia allages grammhs apo br kai p
             global sumbrp
             sumbrp=sumbrp+1
 
